5c5eec82170ef96660db26af6284d81db6f3ddca
Aqua <ollie@ollierobinson.co.uk>
2024-09-30 11:57:56 +0100

    add SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <SDL.h>
 
-#include "Engine/App.h"
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Window* window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    if (!window) {
+        std::cout << "Error creating window: "<< SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    bool alive = true;
+    while (alive) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_WINDOWEVENT: {
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_CLOSE:
+                            alive = false;
+                            break;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
